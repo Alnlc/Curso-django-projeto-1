b4453ecc065bcf72f3038427dd663cd3131ed310
Alan <alanluciodasilvaals@gmail.com>
2025-06-23 10:58:03 -0300

    Test Netify

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render (request,"recipes/pages/home.html", context={
-        'name': 'Zéze de camargo'
-    })
+    return render (request,"recipes/pages/home.html")
